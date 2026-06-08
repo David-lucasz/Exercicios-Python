542e5f87fe0eaf5362493ce97e535ca5c77da76d
DAVID <dividindoconta@gmail.com>
2026-06-08 11:52:38 -0300

    estruturas: if..else..for

---
+++ for1.py
@@ -0,0 +1,7 @@
+#a estrutura de repetiçao ( for ) é usada para
+#ler listas, tuplas, difionarios ou sequencias 
+#neste exemplo usamos uma sequencia com o comando 
+#range(intervalo) que vai de 1 ate 10 e 11 é
+#o valor de saida 
+for i in range(1,11): 
+    print(i)
---
+++ par_impar.py
@@ -0,0 +1,7 @@
+n = input("digite um numero: ")
+n = int(n)
+
+if n % 2 == 0:
+    print("este numero é par")
+else:
+    print("este numero é par")
